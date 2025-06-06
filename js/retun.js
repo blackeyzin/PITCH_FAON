4c8b863464d11ae9c9476dfc5033bf3afb0ed3b6
Nicolas Silva <nicolass.sbsilva@gmail.com>
2025-06-06 13:07:09 -0300

    Create retun.js

---
+++ js/retun.js
@@ -0,0 +1,5 @@
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Escape') {
+    window.location.href = 'index.html'; // altere conforme o nome da sua página principal
+  }
+});
