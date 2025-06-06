614504af2f6c1d3bdd5e1591a0690f8aa1dc462a
Nicolas Silva <nicolass.sbsilva@gmail.com>
2025-06-05 21:46:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function loginGoogle() {
+  // Aqui você pode adicionar autenticação real no futuro
+  window.location.href = "menu.html"; // redireciona para o menu
+}
+
+function loginEmail() {
+  window.location.href = "menu.html";
+}
+
+function loginTwitter() {
+  window.location.href = "menu.html";
+}
