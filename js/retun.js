0d67a75152b172250183db9151ee88787a6a68b5
Nicolas Silva <nicolass.sbsilva@gmail.com>
2025-06-06 16:43:15 -0300

    Update retun.js

--- js/retun.js
+++ js/retun.js
@@ -3,8 +3,8 @@ document.addEventListener('keydown', function(event) {
     const currentPath = window.location.pathname;
 
     // Verifica se a página atual não é a principal
-    if (!currentPath.endsWith('/PITCH_FAON/') && !currentPath.endsWith('/PITCH_FAON/index.html')) {
-      window.location.href = '/PITCH_FAON/';
+    if (!currentPath.endsWith('/PITCH_FAON/') && !currentPath.endsWith('/PITCH_FAON/menu.html')) {
+      window.location.href = '/PITCH_FAON/menu.html';
     }
   }
 });
